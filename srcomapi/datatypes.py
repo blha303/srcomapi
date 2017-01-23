f1667407acab7171ea4fd4c7ef9be8b1eee278c2
Steven Smith <stevensmith.ome@gmail.com>
2017-01-23 17:50:56 +0800

    DataType methods (#1)
    * Testing
    
    * More testing
    
    * DataType methods

--- srcomapi/datatypes.py
+++ srcomapi/datatypes.py
@@ -1,23 +1,36 @@
 from .exceptions import APINotProvidedException
 
 class DataType(object):
-    def __init__(self, srcom=None, data=None, id=None, **kwargs):
+    def __init__(self, srcom=None, data=None, id=None):
+        self._retrieved = []
         self._api = srcom
         if not self._api:
             raise APINotProvidedException("A SpeedrunCom instance was not passed to the DataType")
         if id and not data:
-            self.data = self._api.get("{}/{}".format(endpoint, id), **kwargs)
+            self.data = self._api.get("{}/{}".format(self.endpoint, id))
         elif data:
             self.data = data
-        if self.data and getattr(self, "embeds", None):
+        else:
+            return
+        if self.data:
             for embed in self.embeds:
                 if embed.endpoint in self.data and "data" in self.data[embed.endpoint]:
                     self.data[embed.endpoint] = (embed(embed_data) for embed_data in self.data[embed.endpoint]["data"])
 
     def __getattr__(self, attr):
-        if attr not in self.data:
-            return super(DataType, self).__getattr__(self, attr)
-        return self.data[attr]
+        if not hasattr(self, "data"):
+            raise AttributeError
+        if attr in self.data:
+            if attr in self._api._datatypes:
+                cls = self._api._datatypes[attr]
+                if type(self.data[attr]) is list and len(self.data[attr]) > 0 and len(self.data[attr][0]) == 8:
+                    self.data[attr] = [cls(self._api, data=self._api.get("{}/{}".format(attr,id))) for id in self.data[attr]]
+                    self._retrieved += attr
+                elif type(self.data[attr]) is str and len(self.data[attr]) == 8:
+                    self.data[attr] = cls(self._api, data=self._api.get("{}/{}".format(attr,self.data[attr])))
+                    self._retrieved += attr
+            return self.data[attr]
+        raise AttributeError("{} is not in {}".format(attr, self.data))
 
     def __repr__(self):
         if "name" in self.data:
@@ -25,11 +38,35 @@ class DataType(object):
         elif "id" in self.data:
             repr_str = """<{clsname} "{id}">"""
         else:
-            repr_str = """<{clsname}>"""
+            return """<{clsname}>""".format(clsname=self.__class__.__name__)
         return repr_str.format(clsname=self.__class__.__name__, **self.data)
+
+    @property
+    def embeds(self):
+        return []
 
 class Category(DataType):
     endpoint = "categories"
+
+    @property
+    def variables(self):
+        name = "variables"
+        if name in self._retrieved:
+            return self.data[name]
+        data = [Variable(self._api, data=d) for d in self._api.get("{}/{}/{}".format(self.endpoint, self.id, name))]
+        self.data[name] = data
+        self._retrieved += name
+        return data
+
+    @property
+    def records(self):
+        name = "records"
+        if name in self._retrieved and name in self.data:
+            return self.data[name]
+        data = [Leaderboard(self._api, data=d) for d in self._api.get("{}/{}/{}".format(self.endpoint, self.id, name))]
+        self.data[name] = data
+        self._retrieved += name
+        return data
 
     @property
     def embeds(self):
@@ -45,6 +82,56 @@ class Game(DataType):
     endpoint = "games"
 
     @property
+    def categories(self):
+        name = "categories"
+        if name in self._retrieved:
+            return self.data[name]
+        data = [Category(self._api, data=d) for d in self._api.get("{}/{}/{}".format(self.endpoint, self.id, name))]
+        self.data[name] = data
+        self._retrieved += name
+        return data
+
+    @property
+    def levels(self):
+        name = "levels"
+        if name in self._retrieved:
+            return self.data[name]
+        data = [Level(self._api, data=d) for d in self._api.get("{}/{}/{}".format(self.endpoint, self.id, name))]
+        self.data[name] = data
+        self._retrieved += name
+        return data
+
+    @property
+    def variables(self):
+        name = "variables"
+        if name in self._retrieved:
+            return self.data[name]
+        data = [Variable(self._api, data=d) for d in self._api.get("{}/{}/{}".format(self.endpoint, self.id, name))]
+        self.data[name] = data
+        self._retrieved += name
+        return data
+
+    @property
+    def derived_games(self):
+        name = "derived_games"
+        if name in self._retrieved:
+            return self.data[name]
+        data = [Game(self._api, data=d) for d in self._api.get("{}/{}/{}".format(self.endpoint, self.id, "derived-games"))]
+        self.data[name] = data
+        self._retrieved += name
+        return data
+
+    @property
+    def records(self):
+        name = "records"
+        if name in self._retrieved:
+            return self.data[name]
+        data = [Leaderboard(self._api, data=d) for d in self._api.get("{}/{}/{}".format(self.endpoint, self.id, name))]
+        self.data[name] = data
+        self._retrieved += name
+        return data
+
+    @property
     def embeds(self):
         return [Level, Category, Moderator, GameType, Platform, Region, Genre, Engine, Developer, Publisher, Variable]
 
@@ -58,9 +145,6 @@ class GameType(DataType):
 
 class Genre(DataType):
     endpoint = "genres"
-
-class Guest(DataType):
-    endpoint = "guests"
 
 class Leaderboard(DataType):
     endpoint = "leaderboards"
@@ -93,6 +177,12 @@ class Notification(DataType):
 class Platform(DataType):
     endpoint = "platforms"
 
+class Player(DataType):
+    pass
+
+class Guest(Player):
+    endpoint = "guests"
+
 class Profile(DataType):
     endpoint = "profile"
 
@@ -106,6 +196,21 @@ class Run(DataType):
     endpoint = "runs"
 
     @property
+    def players():
+        if "players" in self._retrieved:
+            return self.data["players"]
+        p = []
+        for player in self.data["players"]:
+            if player["rel"] == "user":
+                p.append(User(self._api, data=player))
+            if player["rel"] == "guest":
+                p.append(Guest(self._api, data=player))
+            else:
+                p.append(Player(self._api, data=player))
+        self.data["players"] = p
+        return p
+
+    @property
     def embeds(self):
         return [Game, Category, Level, Player, Region, Platform]
 
@@ -116,7 +221,7 @@ class Series(DataType):
     def embeds(self):
         return [Moderator]
 
-class User(DataType):
+class User(Player):
     endpoint = "users"
 
 class Moderator(User):
--- srcomapi/srcomapi.py
+++ srcomapi/srcomapi.py
@@ -1,9 +1,11 @@
 import requests
 from http.client import responses
 from os.path import dirname
+from os import environ
+import inspect
 
 import srcomapi
-from .datatypes import *
+import srcomapi.datatypes as datatypes
 from .exceptions import APIRequestException, APINotProvidedException
 
 with open(dirname(srcomapi.__file__) + "/.version") as f:
@@ -12,6 +14,7 @@ API_URL = "https://www.speedrun.com/api/v1/"
 
 class SpeedrunCom(object):
     def __init__(self, api_key=None, user_agent="blha303:srcomapi/"+__version__):
+        self._datatypes = {v.endpoint:v for k,v in inspect.getmembers(datatypes, inspect.isclass) if hasattr(v, "endpoint")}
         self.api_key = api_key
         self.user_agent = user_agent
 
@@ -19,22 +22,18 @@ class SpeedrunCom(object):
         headers = {"User-Agent": self.user_agent}
         if self.api_key:
             headers["X-API-Key"] = self.api_key
-        response = requests.get(API_URL + endpoint, params=kwargs, headers=headers)
+        kwargs.update({"headers": headers})
+        print(API_URL + endpoint)
+        response = requests.get(API_URL + endpoint, **kwargs)
         if response.status_code == 404:
             raise APIRequestException("{} {}".format(response.status_code, responses[response.status_code]), response)
         return response.json()["data"]
 
     def get_game(self, id=None, **kwargs):
         """Returns a generator that yields srcomapi.Game objects containing the response data"""
-        if not "embed" in kwargs:
-            kwargs["embed"] = "categories"
         response = self.get("games" + ("/{}".format(id) if id else ""), **kwargs)
         if type(response) is list:
             for game_data in response:
-                yield Game(self, game_data)
+                yield datatypes.Game(self, data=game_data)
         elif type(response) is dict:
-            yield Game(self, response)
-
-    def get_games(self, **kwargs):
-        """Returns a generator (via SpeedrunCom.get_game) that yields srcomapi.Game objects containing the response data."""
-        return get_game(id=None, **kwargs)
+            yield datatypes.Game(self, data=response)
