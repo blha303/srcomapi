50c5b2355b1d441f98d15da4b827dacccb9f5792
blha303 <stevensmith.ome@gmail.com>
2017-01-24 17:29:07 +0800

    Now with test mocking

--- srcomapi/srcomapi.py
+++ srcomapi/srcomapi.py
@@ -8,27 +8,49 @@ import srcomapi
 import srcomapi.datatypes as datatypes
 from .exceptions import APIRequestException, APINotProvidedException
 
+# libraries for mocking
+import json
+import gzip
+
 with open(dirname(srcomapi.__file__) + "/.version") as f:
     __version__ = f.read().strip()
 API_URL = "https://www.speedrun.com/api/v1/"
 DEBUG = environ.get("DEBUG", None)
+TEST_DATA = dirname(srcomapi.__file__) + "/test_data/"
 
 class SpeedrunCom(object):
-    def __init__(self, api_key=None, user_agent="blha303:srcomapi/"+__version__):
+    def __init__(self, api_key=None, user_agent="blha303:srcomapi/"+__version__, mock=False):
         self._datatypes = {v.endpoint:v for k,v in inspect.getmembers(datatypes, inspect.isclass) if hasattr(v, "endpoint")}
         self.api_key = api_key
         self.user_agent = user_agent
+        self.mock = mock
 
     def get(self, endpoint, **kwargs):
         headers = {"User-Agent": self.user_agent}
         if self.api_key:
             headers["X-API-Key"] = self.api_key
         kwargs.update({"headers": headers})
-        if DEBUG: print(API_URL + endpoint)
-        response = requests.get(API_URL + endpoint, **kwargs)
-        if response.status_code == 404:
-            raise APIRequestException("{} {}".format(response.status_code, responses[response.status_code]), response)
-        return response.json()["data"]
+        uri = API_URL + endpoint
+        if DEBUG: print(uri)
+        if self.mock:
+            mock_endpoint = ".".join(endpoint.split("/")[0::2])
+            try:
+                with gzip.open(TEST_DATA + mock_endpoint + ".json.gz") as f:
+                    data = json.loads(f.read().decode("utf-8"))["data"]
+            except FileNotFoundError:
+                response = requests.get(uri, **kwargs)
+                if response.status_code != 404:
+                    with gzip.open(TEST_DATA + mock_endpoint + ".json.gz", "wb") as f:
+                        f.write(json.dumps(response.json()).encode("utf-8"))
+                    data = response.json()["data"]
+                else:
+                    raise APIRequestException("{} {}".format(response.status_code, responses[response.status_code]), response)
+        else:
+            response = requests.get(uri, **kwargs)
+            if response.status_code == 404:
+                raise APIRequestException("{} {}".format(response.status_code, responses[response.status_code]), response)
+            data = response.json()["data"]
+        return data
 
     def get_game(self, id, **kwargs):
         """Returns a srcomapi.datatypes.Game object"""
