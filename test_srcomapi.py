f51e3bc7caa619eb3d57863b56ecee3bb8792d33
blha303 <stevensmith.ome@gmail.com>
2017-01-24 14:40:34 +0800

    More tests

--- test_srcomapi.py
+++ test_srcomapi.py
@@ -1,6 +1,21 @@
 import srcomapi
 import gzip
+import json
+
+def load(fn):
+    with gzip.open("test_data/{}.json.gz".format(fn)) as f:
+        return json.loads(f.read().decode("utf-8"))["data"]
+
+def setup_game():
+    data = load("games")
+    api = srcomapi.SpeedrunCom()
+    return srcomapi.datatypes.Game(api, data=data)
 
 def test_api_get_game():
     api = srcomapi.SpeedrunCom()
     assert(api.get_game("sms").name == "Super Mario Sunshine")
+
+def test_game():
+    game = setup_game()
+    assert(game.name == "Super Mario Sunshine")
+    assert(game.abbreviation == "sms")
