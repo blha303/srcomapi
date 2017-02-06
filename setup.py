2101deda48b44aa050253b51df8e269a02d23446
blha303 <stevensmith.ome@gmail.com>
2017-02-06 19:41:45 +0800

    Add .version, fixes #3

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     name='srcomapi',
     version=version,
     packages=['srcomapi'],
+    package_data={'': ['.version']},
     include_package_data=True,
     install_requires=[
         'requests'
