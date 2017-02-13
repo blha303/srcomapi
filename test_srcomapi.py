32b07cc98be60ded1a38fab56fdc5fbfcd2e079e
blha303 <stevensmith.ome@gmail.com>
2017-02-14 03:01:53 +0800

    Fix tests

--- test_srcomapi.py
+++ test_srcomapi.py
@@ -3,12 +3,13 @@ import srcomapi
 def setup_api():
     return srcomapi.SpeedrunCom(mock=True)
 
-def test_speedruncom_get_game():
+def test_search_for_game():
     api = setup_api()
-    assert(api.get_game("sms").name == "Super Mario Sunshine")
-
-def test_game():
-    api = setup_api()
-    game = srcomapi.datatypes.Game(api, id=id)
+    game = api.search(srcomapi.datatypes.Game, {"name": "super mario sunshine"})[0]
     assert(game.name == "Super Mario Sunshine")
-    assert(game.abbreviation == "sms")
+
+def test_world_record():
+    api = setup_api()
+    game = srcomapi.datatypes.Game(api, id="v1pxjz68")
+    record = game.categories[0].records[0]
+    assert( isinstance(record.runs[0]["run"], srcomapi.datatypes.Run) )
